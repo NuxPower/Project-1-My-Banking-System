78eff1c0ab7b735b180b37716d72071e494c37f7
JohnGabz <159271723+JohnGabz@users.noreply.github.com>
2024-03-07 10:32:09 +0800

    Update AccountLauncher.java

--- Accounts/AccountLauncher.java
+++ Accounts/AccountLauncher.java
@@ -10,11 +10,32 @@ public class AccountLauncher {
     }
 
     public static void accountLogin() {
-
+        
     }
 
+    /**
+     * Bank selection screen before the user is prompted to login. User is prompted for the Bank ID
+     * with corresponding bank name.
+     * 
+     * @return Bank object based on selected ID
+     */
     private static Bank selectBank() {
-        return null;
+        int bankID = Integer.parseInt(Main.prompt("Enter bank ID: ", true));
+        String bankName = Main.prompt("Enter bank name: ", true);
+        Bank selbank = null;
+        
+        for (Bank bank : BankLauncher.getBANKS()) {
+            if (bank.getID() == bankID && bank.getName().equals(bankName)) {
+                selbank = bank;
+                System.out.println("Bank selected: " + bankName);
+                break;
+            } else {
+                System.out.println("Bank does not exist.");
+                return null;
+            }
+        }
+
+        return selbank;
     }
 
     private static void setLogSession() {
@@ -25,8 +46,23 @@ public class AccountLauncher {
 
     }
 
-    public Account checkCredentials() {
-        return null;
+    /**
+     * Checks inputted credentials during account login.
+     * 
+     * @param accountNum – Account number.
+     * @param pin – 4-digit pin.
+     * 
+     * @return Account object if it passes verification. Null if not.
+     */
+    public static Account checkCredentials(String accountNum, String pin) {
+        Account selAccount = assocBank.getBankAccount(assocBank, accountNum);
+        System.out.println(selAccount.toString());
+        if (selAccount != null && selAccount.getACCOUNTNUMBER().equals(accountNum) && selAccount.getPin().equals(pin)) {
+            return selAccount;
+        } else {
+            System.out.println("Invalid account number or PIN.");
+            return null;
+        }
     }
 
     protected static CreditAccount getLoggedAccount() {
