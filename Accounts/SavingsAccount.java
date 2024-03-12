acdd1d4ef6b66f8699ae8e9ef6347f0a109c918e
JohnGabz <159271723+JohnGabz@users.noreply.github.com>
2024-03-12 12:09:30 +0800

    Update SavingsAccount.java

--- Accounts/SavingsAccount.java
+++ Accounts/SavingsAccount.java
@@ -15,12 +15,28 @@ public class SavingsAccount extends Account {
         return false;
     }
 
+    /*
+     * Warns the account owner that their balance is not enough for the transaction to proceed
+     * successfully.
+     */
     private void insufficientBalance() {
-        
+        System.out.println("Insufficient balance for the transaction. Please check your account balance.");
     }
 
-    private void adjustAccountBalance(double amount) {
-
+    /*
+     * Adjust the account balance of this savings account based on the amount to be adjusted. If it
+     * results to the account balance going less than 0.0, then it is forcibly reset to 0.0.
+     * 
+     * @param amount – Amount to be added or subtracted from the account balance.
+     */
+    public void adjustAccountBalance(double amount) {
+        if (hasEnoughBalance(amount) == true) {
+            this.balance += amount;
+            System.out.println("Transaction succesful");
+        } else {
+            insufficientBalance();
+            amount = 0.0;
+        }
     }
 
     public String toString() {
