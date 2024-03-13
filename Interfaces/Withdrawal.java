6c0ae359c57f05ceed0dc361a70a16d055c694ee
NuxPower <108980680+NuxPower@users.noreply.github.com>
2024-03-13 13:08:11 +0800

    changed the structure

--- Interfaces/Withdrawal.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-public interface Withdrawal {
-    public String getAccountBalance();
-    boolean hasEnoughBalance(double amount);
-    void insufficientBalance();
-    void adjustAccountBalance(double amount);
-}
