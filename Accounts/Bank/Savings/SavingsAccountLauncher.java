4d4f1a1df1aab908c611e8edb37e56010032068a
Kiks <161555954+Kiks777@users.noreply.github.com>
2024-03-16 11:52:59 +0800

    Update SavingsAccountLauncher.java
    private static void withdrawProcess() {...}

--- Accounts/Bank/Savings/SavingsAccountLauncher.java
+++ Accounts/Bank/Savings/SavingsAccountLauncher.java
@@ -10,8 +10,36 @@ public class SavingsAccountLauncher extends AccountLauncher {
 
     }
 
+   //  Janos and Mia here
     private static void withdrawProcess() {
+
+        SavingsAccount loggedAccount = getLoggedAccount();
+        if (loggedAccount != null) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter amount to withdraw: ");
+            double amount = scanner.nextDouble();
+            
+
+            if (amount <= 0) {
+                System.out.println("Invalid amount. Please enter a positive value.");
+                return;
+            }
+            
         
+            if (loggedAccount.hasEnoughBalance(amount)) {
+            
+                if (loggedAccount.withdrawal(amount)) { 
+                    System.out.println("Withdrawal successful");
+                } else {
+                    System.out.println("Withdrawal failed");
+                }
+            } else {
+                System.out.println("Insufficient balance for withdrawal.");
+            }
+        } else {
+            System.out.println("No account logged in.");
+        }
     }
 
     private static void fundTransferProcess() {
