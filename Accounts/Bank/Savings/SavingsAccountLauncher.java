1854d191566337e5d96a18a478d837e4fd7819ce
heheboi1534 <160028886+heheboi1534@users.noreply.github.com>
2024-03-16 14:02:27 +0800

    Update SavingsAccountLauncher.java
    savingsAccountInit()

--- Accounts/Bank/Savings/SavingsAccountLauncher.java
+++ Accounts/Bank/Savings/SavingsAccountLauncher.java
@@ -4,13 +4,21 @@ import Account.AccountLauncher;
 public class SavingsAccountLauncher extends AccountLauncher {
     
      // Janos and Mia here
+    /**
+     * Initializes the savings account functionality for the currently logged-in user.
+     * Prints the account balance statement if a savings account is logged in.
+     * Otherwise, prints a message indicating that no account is logged in.
+     */
     public static void savingsAccountInit() {
-        
+        // Retrieve the currently logged-in savings account
         SavingsAccount loggedAccount = getLoggedAccount();
+
+         // Check if a savings account is logged in
         if (loggedAccount != null) {
+            // Print welcome message and account balance statement
             System.out.println("Welcome to your savings account!");
             System.out.println(loggedAccount.getAccountBalanceStatement());
-            
+            // Print message indicating no account is logged in
         } else {
             System.out.println("No account logged in.");
         }
