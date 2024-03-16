bf608783ef43e937e4371001ea002dd4b37de2e3
JcZz112 <161596807+JcZz112@users.noreply.github.com>
2024-03-16 20:13:38 +0800

    Update SavingsAccountLauncher.java

--- Accounts/Bank/Savings/SavingsAccountLauncher.java
+++ Accounts/Bank/Savings/SavingsAccountLauncher.java
@@ -7,7 +7,37 @@ public class SavingsAccountLauncher extends AccountLauncher {
     }
 
     private static void depositProcess() {
+        SavingsAccount savingsAccount = getLoggedAccount();
 
+        if (savingsAccount == null) {
+            System.out.println("No account found!");
+            return;
+        }
+
+        double depositAmount;
+
+        while (true) {
+            String depositAmount_str = Main.prompt("Enter deposit amount: ", true);
+
+            try {
+                depositAmount = Double.parseDouble(depositAmount_str);
+                if (depositAmount <= 0) {
+                    System.out.println("Invalid amount!");
+                } else {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid amount!");
+            }
+        }
+
+        boolean depositSuccess = savingsAccount.cashDeposit(depositAmount);
+        if (depositSuccess) {
+            System.out.println("Deposit successful!");
+            System.out.println("Balance: " + savingsAccount.getBalance());
+        } else {
+            System.out.println("Deposit failed. Please try again.");
+        }
     }
 
     private static void withdrawProcess() {
