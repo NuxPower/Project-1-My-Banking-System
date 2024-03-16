f42904a1085e639de8df69ef2740d27661624f7f
Kiks <161555954+Kiks777@users.noreply.github.com>
2024-03-16 12:46:19 +0800

    Update CreditAccountLauncher.java
    public static void creditAccountInit()

--- Accounts/Bank/Credit/CreditAccountLauncher.java
+++ Accounts/Bank/Credit/CreditAccountLauncher.java
@@ -1,10 +1,57 @@
 package Bank.Credit;
 import Account.AccountLauncher;
 
-public class CreditAccountLauncher extends AccountLauncher {
-    public static void creditAccountInit() {
+// JAnos and Mia
+import Bank.BankLauncher;
+import Main.Main;
 
-    }
+public class CreditAccountLauncher extends AccountLauncher {
+    
+        // Janos and Mia 
+        // Mia naay 3 ka error dri dnko ma solve
+        public static void creditAccountInit() {
+            AccountLauncher.accountLogin();
+
+            while (AccountLauncher.isLoggedIn()) {
+                Main.showMenuHeader("Credit Account Menu");
+                Main.showMenu(41);
+                Main.setOption();
+
+                switch (Main.getOption()) {
+                    case 1:
+                        System.out.println(getLoggedAccount().getLoanStatement());
+                        break;
+                    case 2:
+                        double paymentAmount = Double.parseDouble(Main.prompt("Enter payment amount: ", true));
+                        boolean paymentSuccess = getLoggedAccount().pay(paymentAmount);
+                        if (paymentSuccess) {
+                            System.out.println("Payment successful.");
+                        } else {
+                            System.out.println("Payment failed. Insufficient funds.");
+                        }
+                        break;
+                    case 3:
+                        double recompenseAmount = Double.parseDouble(Main.prompt("Enter recompense amount: ", true));
+                        boolean recompenseSuccess = getLoggedAccount().recompense(recompenseAmount);
+                        if (recompenseSuccess) {
+                            System.out.println("Recompense successful.");
+                        } else {
+                            System.out.println("Recompense failed. Exceeded credit limit.");
+                        }
+                        break;
+                    case 4:
+                        System.out.println(getLoggedAccount().getTransactionsInfo());
+                        break;
+                    case 5:
+                        System.out.println("Logging out from Credit Account.");
+                        destroyLogSession();
+                        return;
+                    default:
+                        System.out.println("Invalid option!");
+                        break;
+                }
+            }
+        }
 
     private static void creditPaymentProcess() {
 
